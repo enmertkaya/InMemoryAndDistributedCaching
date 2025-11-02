75b22e20cd07886eaa269caa267f9a0fd561f30c
Enes Mert Kaya <151652097+enmertkaya@users.noreply.github.com>
2025-11-02 21:20:16 +0300

    Add project files.

---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("set/{name}")]
+        public void Set(string name)
+        {
+            _memoryCache.Set("name",name);
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            if (_memoryCache.TryGetValue<string>("name", out var name) && name != null)
+            {
+                return name.Length > 3 ? name.Substring(3) : string.Empty;
+            }
+
+            return string.Empty;
+        }
+    }
+}
